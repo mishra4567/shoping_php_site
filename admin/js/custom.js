5d51329bcbe828d6fdcabc2a4725bbef2823fe42
mishra4567 <mishrapritam831@gmail.com>
2024-07-10 10:50:48 +0530

    bug fex

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -1,29 +1,19 @@
-function get_sub_cat(sub_cat_id){
-    var categories_id=jQuery('#categories_id').val();
+function get_sub_cat(sub_cat_id) {
+    var categories_id = jQuery('#categories_id').val();
     // alert(categories_id)
     jQuery.ajax({
-        url:'get_sub_cat.php',
-        type:'post',
-        data:'categories_id='+categories_id+'&sub_cat_id='+sub_cat_id,
-        success:function(result){
+        url: 'get_sub_cat.php',
+        type: 'post',
+        data: 'categories_id=' + categories_id + '&sub_cat_id=' + sub_cat_id,
+        success: function (result) {
             jQuery('#sub_categories_id').html(result);
         }
     })
 }
 // select img
-let profilePic=document.getElementById("profilePic");
-let InputFile=document.getElementById("input-img");
+let profilePic = document.getElementById("profilePic");
+let InputFile = document.getElementById("input-img");
 
-InputFile.onchange=function(){
-    profilePic.src=URL.createObjectURL(InputFile.files[0]);
-}
-// multi select
-$(document).ready(function(){
-    $("#checkAll").click(function(){
-        if($(this).is(":checked")){
-            $(".checkItem").prop('checked',true);
-        }else{
-            $(".checkItem").prop('checked',false);
-        }
-    });
-});
+InputFile.onchange = function () {
+    profilePic.src = URL.createObjectURL(InputFile.files[0]);
+} 
